35fdfb069358cd3e89de4622b36127691549f602
RaysChen <543114160@qq.com>
2016-03-31 16:23:07 +0800

    Frame
    修改AppDelegate.m文件
    创建根控制器

--- 快速搭建框架实验/快速搭建框架实验/AppDelegate.m
+++ 快速搭建框架实验/快速搭建框架实验/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CXTabbarController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //1.创建窗口
+    self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    //2.设置窗口的根控制器
+    self.window.rootViewController = [[CXTabbarController alloc]init];
+    
+    //3.显示窗口
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
